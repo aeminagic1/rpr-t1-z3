6038e87f591f5579defbfbac923264b57452274b
Anesa Eminagic <aeminagic1@etf.unsa.ba>
2021-10-18 13:17:34 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Sat satic=new Sat(15,30,45);
+    satic.Ispisi();
+    satic.Sljedeci();
+    satic.Ispisi();
+    satic.PomjeriZa(-48);
+    satic.Ispisi();
+    satic.Postavi(0,0,0);
+    satic.Ispisi();
+
+    }
+}
